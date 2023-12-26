d0aeda688cbf6c10b28cf1338d582c144c3b9670
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2023-12-26 15:33:20 +0100

    remove splitting

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   target: 'es2020',
   format: ['esm', "cjs"],
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
