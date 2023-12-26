367b75776f39ac378e5c485451a089d136640a21
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2023-12-26 15:29:27 +0100

    update builder

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   target: 'es2020',
-  format: ['esm'],
+  format: ['esm', "cjs"],
   splitting: true,
   sourcemap: true,
   clean: true,
